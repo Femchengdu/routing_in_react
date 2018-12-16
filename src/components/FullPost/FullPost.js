d8269975292df8639be0037d4fbe5c4f91e2f5d3
Femchengdu <femiajayi705@gmail.com>
2018-12-16 18:49:38 +0000

    Fetched data in componentDidUpdate without creating an infinite loop.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        detailedPost: null
+    }
+    componentDidUpdate () {
+        if (this.props.id) {
+            if (!this.state.detailedPost || (this.state.detailedPost && this.state.detailedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        //console.log(response.data);
+                        this.setState({detailedPost: response.data});
+                    }  );
+            };
+
+            // if (this.state.detailedPost && this.state.detailedPost.id !== this.props.id) {
+            // axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            //     .then(response => {
+            //         //console.log(response.data);
+            //         this.setState({detailedPost: response.data});
+            //     } );
+            // };
+            
+        }
+    }
     render () {
         let post = <p style={{textAlign: 'center'}}>Select a Post!</p>;
 
         if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading.....</p>;
+        }
+
+        if (this.state.detailedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.detailedPost.title}</h1>
+                    <p>{this.state.detailedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
