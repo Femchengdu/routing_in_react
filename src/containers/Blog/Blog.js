0aeef26086e93b5d8b27f3df850379fbfe03d037
Femchengdu <femiajayi705@gmail.com>
2018-12-16 15:04:27 +0000

    Rendered the fetched data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,32 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(
+                // Call setState inside the then methond becase the call to the server is asynchronous
+                response => {
+                    this.setState({posts: response.data})
+                    //console.log(response);
+                }
+            );  
+    }
+    
     render () {
+        const postData = this.state.posts.map( singlePost => {
+            return <Post key={singlePost.id} title={singlePost.title} />
+        }
+        );
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {postData}
                 </section>
                 <section>
                     <FullPost />
