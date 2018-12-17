e5e8da56be9b8bb6a396ad80b9bc96ce6b92e356
Femchengdu <femiajayi705@gmail.com>
2018-12-17 08:45:50 +0000

    Added global request and response axios interceptors to the index file.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 import * as serviceWorker from './serviceWorker';
+
+axios.interceptors.request.use(request => {
+	console.log(request);
+	// Always return the request, you can also edit it before returning it.
+	return request
+}, error => {
+	console.log(error);
+	return Promise.reject(error);
+});
+
+// It is important to use the response keyword here
+
+axios.interceptors.response.use(response => {
+	console.log(response);
+	// Always return the request, you can also edit it before returning it.
+	return response
+}, error => {
+	console.log(error);
+	return Promise.reject(error);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
